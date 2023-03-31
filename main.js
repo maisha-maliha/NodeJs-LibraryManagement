c3a3b891c000c6f0ea58888ba886a937bdf6e9eb
fla falf alf <officiallymaliha@gmail.com>
2023-04-01 00:22:56 +0600

    adding server and mysql

--- main.js
+++ main.js
@@ -1,17 +1,28 @@
-//const mysql = require('mysql');
 const http =  require('http');
+const fs = require('fs');
 
-const server = http.createServer((req, res)=>{
-    res.write("hello maisha ");
-    if(req.url == '/'){
-        res.write("you are in home page " + req.url);
-        res.end();
-    }
-    if(req.url == '/books'){
-        res.write("you are in boooks");
-        res.end();
-    }
+const data = fs.readFileSync("./FrontEnd/templates/index.html", "utf-8");
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'maisha',
+  password : 'maisha30',
+  database : 'store_book'
 });
+connection.connect();
+// SERVER
+const server = http.createServer((req, res)=>{
+    res.writeHead(200,{'Content-Type':'text/html'});
+    connection.query('SELECT book_name FROM books', function (error, results, fields) {
+        if (error) throw error;
+        console.log('The solution is: ', results[0]);
+      });
+    res.end();
+});
+ 
+ 
+connection.end();
 
 server.listen(3000);
-console.log("i can see you");
+console.log("it works");
