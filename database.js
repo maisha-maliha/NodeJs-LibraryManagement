6ea4336d6c46c79edc3ef960f6d00872c0f83aba
maisha-maliha <officiallymaliha@gmail.com>
2023-04-02 23:29:40 +0600

    working with static file routing

--- database.js
+++ database.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 // DATABASE
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
@@ -7,20 +9,22 @@ var connection = mysql.createConnection({
   database : 'store_book'
 });
 connection.connect();
-var booksname = '<ul>';
-connection.query('SELECT book_name FROM books;', function (error, results, fields) {
+
+var booksname = '<section><tr><th>NAME</th><th>CATAGORY</th><th>PRICE</th><th>QUANTITY</th></tr>';
+
+connection.query('SELECT * FROM books;', function (error, results, fields) {
   if (error) throw error;
   results.forEach(element => {
-    booksname += '<li>'+ element.book_name + '</li>';
+    booksname += '<tr>'+ '<td>'+element.book_name+'</td>'+'<td>'+element.catagory+'</td>'+'<td>'+element.price+'</td>'+'<td>'+element.quantity+'</td>'+ '</tr>';
   });
-  booksname += '</ul>'
+  booksname += '</section><script src="../script/script.js"></script>';
+  fs.writeFile('./FrontEnd/templates/booklist.html',booksname, err =>{if(err) console.log(err)});
   //console.log(booksname);
 });
 // the following code will not show the booksname result
 // becuase it is outside the connection.query() function
 // module.exports = { myval : booksname};
 // console.log(booksname); // this shows <ul>
-console.log(booksname);
-module.exports = {book : booksname};
+//module.exports = {book : booksname};
 //module.exports = {book : booksname};
 connection.end();
--- main.js
+++ main.js
@@ -1,15 +1,47 @@
 const http =  require('http');
 const fs = require('fs');
+const path = require('path');
+
 const data = fs.readFileSync("./FrontEnd/templates/index.html", "utf-8");
 
 // DATA
 const {book} = require('./database.js');
-console.log(book)
 //data.write(myval)
 // SERVER
 const server = http.createServer((req, res)=>{
-    res.writeHead(200,{'Content-Type':'text/html'});
-    res.write();
+    //console.log(req.url);
+    if(req.url == '/'){
+        res.writeHead(200,{'Content-Type':'text/html'});
+        res.write(data);
+    }
+    if(req.url == '/script/script.js'){
+        let filePath = path.join(__dirname,"FrontEnd", req.url);
+        fs.readFile(filePath, (err, content) =>{
+            if (err) console.log(err);
+            else {
+                res.writeHead(200, {'Content-Type': 'text/javascript'});
+                let data = content.toString();
+                console.log(data);
+            }
+        });
+        //const readStream = fs.createReadStream(filePath, "utf-8");
+        //readStream.pipe(res);
+    }
+    if(req.url == '/styles/style.css'){
+        let filePath = path.join(__dirname,"FrontEnd", req.url);
+        fs.readFile(filePath, (err, content) =>{
+            if (err) console.log(err);
+            else {
+                res.writeHead(200, {'Content-Type': 'text/css'});
+                //console.log(filePath);
+                let data = content.toString();
+                console.log(data);
+                res.end(data);
+            }
+        });
+        //const readStream = fs.createReadStream(filePath, "utf-8");
+        //readStream.pipe(res);
+    }
     res.end();
 });
 
