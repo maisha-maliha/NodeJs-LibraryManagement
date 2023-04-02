f8040fb7244a30b9d23320e39af52487b4dfb88c
maisha-maliha <officiallymaliha@gmail.com>
2023-04-03 00:06:07 +0600

    succesfully learned to add statics file to node ueing routing

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 const http =  require('http');
 const fs = require('fs');
-const path = require('path');
-
-const data = fs.readFileSync("./FrontEnd/templates/index.html", "utf-8");
+//const path = require('path');
 
 // DATA
 const {book} = require('./database.js');
@@ -12,38 +10,19 @@ const server = http.createServer((req, res)=>{
     //console.log(req.url);
     if(req.url == '/'){
         res.writeHead(200,{'Content-Type':'text/html'});
+        let data = fs.readFileSync("./FrontEnd/templates/index.html", "utf-8");
         res.write(data);
     }
     if(req.url == '/script/script.js'){
-        let filePath = path.join(__dirname,"FrontEnd", req.url);
-        fs.readFile(filePath, (err, content) =>{
-            if (err) console.log(err);
-            else {
-                res.writeHead(200, {'Content-Type': 'text/javascript'});
-                let data = content.toString();
-                console.log(data);
-            }
-        });
-        //const readStream = fs.createReadStream(filePath, "utf-8");
-        //readStream.pipe(res);
+        res.writeHead(200,{'Content-Type':'text/javascript'});
+        let data = fs.readFileSync("./FrontEnd/script/script.js", "utf-8");
+        res.end(data);
     }
     if(req.url == '/styles/style.css'){
-        let filePath = path.join(__dirname,"FrontEnd", req.url);
-        fs.readFile(filePath, (err, content) =>{
-            if (err) console.log(err);
-            else {
-                res.writeHead(200, {'Content-Type': 'text/css'});
-                //console.log(filePath);
-                let data = content.toString();
-                console.log(data);
-                res.end(data);
-            }
-        });
-        //const readStream = fs.createReadStream(filePath, "utf-8");
-        //readStream.pipe(res);
+        res.writeHead(200,{'Content-Type':'text/css'});
+        let data = fs.readFileSync("./FrontEnd/styles/style.css", "utf-8");
+        res.end(data);
     }
     res.end();
-});
+}).listen(3000);
 
-server.listen(3000);
-//console.log(myval);
